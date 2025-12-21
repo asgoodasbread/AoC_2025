6e6ae969db3cc304863f47a81d2dd655150ab478
asgoodasbread <hardcorepl307@gmail.com>
2025-12-22 00:44:39 +0100

    Day2 done

--- Day2/Day2.go
+++ Day2/Day2.go
@@ -3,6 +3,7 @@ package main
 import (
 	"adventofcode2025/utils"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -13,14 +14,47 @@ type ID struct {
 
 func main() {
 	part_1()
-	//part_2()
+	part_2()
+}
+
+func part_2() {
+	input := utils.ReadInputSep("Day2/input_day2.txt", ",")
+	IDs := parse_ids(input)
+	//fmt.Println(IDs)
+	res := 0
+	for _, id := range IDs {
+		for i := id.lbound; i <= id.hbound; i++ {
+			si := strconv.Itoa(i)
+			if is_repeated(si) {
+				res += i
+			}
+		}
+	}
+	fmt.Println(res)
+}
+
+func is_repeated(s string) bool {
+	doubleS := s + s
+	return strings.Contains(doubleS[1:len(doubleS)-1], s)
 }
 
 func part_1() {
-	input := utils.ReadInputSep("Day2/test_day2.txt", ",")
+	input := utils.ReadInputSep("Day2/input_day2.txt", ",")
 	IDs := parse_ids(input)
-	fmt.Println(IDs)
-
+	//fmt.Println(IDs)
+	res := 0
+	for _, id := range IDs {
+		for i := id.lbound; i <= id.hbound; i++ {
+			cs := strconv.Itoa(i)
+			//fmt.Println(cs)
+			if len(cs)%2 == 0 {
+				if cs[:len(cs)/2] == cs[len(cs)/2:] {
+					res += i
+				}
+			}
+		}
+	}
+	fmt.Println(res)
 }
 
 func parse_ids(input []string) []ID {
